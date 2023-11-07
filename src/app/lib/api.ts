47c076588923bd3e107652a797c8b4d82522cf2f
david-mendes-s <david.a.mendes.s@gmail.com>
2023-11-07 11:46:18 -0300

    update interfaces typescript

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://task-manager-production-15c6.up.railway.app', // Insira a URL da API de desenvolvimento
+    baseURL: 'https://task-manager-production-15c6.up.railway.app/', // Insira a URL da API de desenvolvimento
 });
 
 export default api;
